ad6d1e284a09e5962c4facf08ae16a0e01f35425
AlyssaEdwards13 <edwardsalyssa83@gmail.com>
2018-06-05 17:52:27 +0000

    automatically backed up by learn

---
+++ greeting.rb
@@ -0,0 +1,7 @@
+def greeting(local_greeting, your_name, my_name, time_of_day)
+  
+  puts "#{local_greeting} #{your_name}, I am #{my_name}, hows your #{time_of_day}?"
+
+end 
+
+greeting("hey", "shlurrr", "shlurrr", "morning")
---
+++ wake_up.rb
@@ -0,0 +1,7 @@
+def wake_up
+  puts "wake up then fall right back asleep"
+  puts "mom comes into the room and wakes me up"
+  puts "dog jumps onto my bed and lays on me"
+  puts "my big sister comes in and asks me if I want food"
+end 
+wake_up
